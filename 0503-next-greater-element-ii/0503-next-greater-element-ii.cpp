a65cc1f8755ee18ecac478fcd4b8d2d767497803
Satwik Mushini <satwikmushini@gmail.com>
2026-06-21 19:07:19 +0530

    Time: 7 ms (35.57%), Space: 28.7 MB (21.83%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(n, -1);
+        stack<int> stk;
+        for(int i = 2 * n - 1; i >= 0; i--){
+            while(!stk.empty() && stk.top() <= nums[i % n]){
+                stk.pop();
+            }
+            ans[i % n] = stk.empty() ? -1 : stk.top();
+            stk.push(nums[i % n]);
+        }
+        return ans;
+    }
+};
