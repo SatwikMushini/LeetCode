6804c81b9ea63c7b76f0a1f110d0fdee1a845087
Satwik Mushini <satwikmushini@gmail.com>
2026-06-23 18:18:21 +0530

    Time: 1305 ms (5.14%), Space: 81.3 MB (71.74%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int>NSE(n, n);
+        int n = heights.size(), ans = 0;
         stack<int>stk;
-        for(int i = n-1; i >= 0; i--){
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop();
+        for(int i = 0; i < n; i++){
+            while(!stk.empty() && heights[stk.top()] > heights[i]){
+                int idx = stk.top(); stk.pop();
+                int leftSmallerIdx = stk.empty() ? -1 : stk.top();
+                ans = max(ans, heights[idx] * (i - leftSmallerIdx - 1));
+                cout << ans << " ";
             }
-            NSE[i] = stk.empty() ? n : stk.top();
             stk.push(i);
         }
-        int ans = 0, leftSmallerIdx = -1;
-        stk = stack<int>();
-        for(int i = 0; i < n; i++){
-            while(!stk.empty() && heights[stk.top()] >= heights[i]){
-                stk.pop();
-            }
-            leftSmallerIdx = stk.empty() ? -1 : stk.top();
-            stk.push(i);
-
-            ans = max(ans, heights[i] * (NSE[i] - leftSmallerIdx - 1));
+        while(!stk.empty()){
+            int idx = stk.top(); stk.pop();
+            int leftSmallerIdx = stk.empty() ? -1 : stk.top();
+            ans = max(ans, heights[idx] * (n - leftSmallerIdx - 1));
+            cout << ans << " ";
         }
         return ans;
     }
