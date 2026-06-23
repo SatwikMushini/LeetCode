ea1d1e2a68962294bac60f025944992ba1490597
Satwik Mushini <satwikmushini@gmail.com>
2026-06-23 18:19:32 +0530

    Time: 7 ms (98.16%), Space: 81.5 MB (60.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -8,7 +8,6 @@ public:
                 int idx = stk.top(); stk.pop();
                 int leftSmallerIdx = stk.empty() ? -1 : stk.top();
                 ans = max(ans, heights[idx] * (i - leftSmallerIdx - 1));
-                cout << ans << " ";
             }
             stk.push(i);
         }
@@ -16,7 +15,6 @@ public:
             int idx = stk.top(); stk.pop();
             int leftSmallerIdx = stk.empty() ? -1 : stk.top();
             ans = max(ans, heights[idx] * (n - leftSmallerIdx - 1));
-            cout << ans << " ";
         }
         return ans;
     }
