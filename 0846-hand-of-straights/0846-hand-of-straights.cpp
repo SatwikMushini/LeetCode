59216602eb0ceba179f0e6ef6c50380d497616e2
Satwik Mushini <satwikmushini@gmail.com>
2026-07-09 20:53:23 +0530

    Time: 40 ms (49.27%), Space: 32.1 MB (78.05%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -2,31 +2,17 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size() % groupSize != 0)return false;
-        unordered_map<int,int> freq;
+        map<int,int> freq;
         for(auto x : hand){
             freq[x]++;
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        for(auto &[key, value]: freq){
-            pq.push({key, value});
-        }
-        while(!pq.empty()){
-            int group = groupSize;
-            vector<pair<int,int>>temp;
-            int prevVal = -1;
-            while(group && !pq.empty()){
-                auto topi = pq.top(); pq.pop();
-                if(topi.second > 1)temp.push_back({topi.first, topi.second-1});
-                if(prevVal == -1)prevVal = topi.first;
-                else{
-                    if(topi.first - prevVal != 1)return false;
-                    prevVal = topi.first;
-                }
-                group--;
-            }
-            if(group)return false;
-            for(auto x : temp){
-                pq.push(x);
+        while(!freq.empty()){
+            int key = freq.begin()->first;
+            for(int i = 0; i < groupSize; i++){
+                if(freq.find(key + i) == freq.end())return false;
+
+                freq[key + i]--;
+                if(freq[key + i] == 0)freq.erase(key+i);
             }
         }
         return true;
