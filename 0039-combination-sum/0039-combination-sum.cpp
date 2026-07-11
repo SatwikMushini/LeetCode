ede46f23cd8185fa11ca641c71acfeb990476e65
Satwik Mushini <satwikmushini@gmail.com>
2026-07-11 18:34:23 +0530

    Time: 0 ms (100%), Space: 14.1 MB (76.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void findSum(vector<int>&arr, int idx, int sum, vector<int>&store, vector<vector<int>>& ans){
-        if(sum <= 0){
-            if(sum == 0){
-                ans.push_back(store);
+    void find(vector<int>&arr, int idx, int target, vector<int>&store, vector<vector<int>>&ans){
+        for(int i = idx; i < arr.size(); i++){
+            store.push_back(arr[i]);
+            if(target-arr[i] <= 0){
+                if(target-arr[i] == 0){
+                    ans.push_back(store);
+                }
+                store.pop_back();
+                break;
             }
-            return;
+            find(arr, i, target-arr[i], store, ans);
+            store.pop_back();
         }
-        if(idx == arr.size())return;
-        store.push_back(arr[idx]);
-        findSum(arr, idx, sum-arr[idx], store, ans);
-        store.pop_back();
-        findSum(arr, idx+1, sum, store, ans);
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& cads, int target) {
+        sort(cads.begin(), cads.end());
         vector<vector<int>> ans;
         vector<int>store;
-        findSum(candidates, 0, target, store, ans);
+        find(cads, 0, target, store, ans);
         return ans;
     }
 };
