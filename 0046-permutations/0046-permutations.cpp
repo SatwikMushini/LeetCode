91009d99f66a73f278c6754fe1dea750a1980e01
Satwik Mushini <satwikmushini@gmail.com>
2026-07-13 12:03:18 +0530

    Time: 0 ms (100%), Space: 10.7 MB (51.1%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void findPermutations(vector<int>&nums, vector<int>&store, vector<int>&visited, vector<vector<int>>& ans){
-        if(store.size() == nums.size()){
-            ans.push_back(store);
-            return;
-        }
-
-        for(int i = 0; i < nums.size(); i++){
-            if(visited[nums[i] + 10])continue;
-
-            visited[nums[i] + 10] = 1;
-            store.push_back(nums[i]);
-
-            findPermutations(nums, store, visited, ans);
-
-            visited[nums[i] + 10] = 0;
-            store.pop_back();
+    void run(vector<int>& arr, int i, vector<vector<int>>& nums){
+        nums.push_back(arr);
+        if(i == arr.size() - 1)return;
+        
+        for(int k = i; k < arr.size()-1; k++){
+            for(int j = k+1; j < arr.size(); j++){
+                swap(arr[k], arr[j]);
+                run(arr, k+1, nums);
+                swap(arr[k], arr[j]);
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int>visited(21, 0);
-        vector<int>store;
-        findPermutations(nums, store, visited, ans);
+        run(nums, 0, ans);
         return ans;
     }
 };
