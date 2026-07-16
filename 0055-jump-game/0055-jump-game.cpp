bd6a04f20144648603f972f7376c67489a29307d
Satwik Mushini <satwikmushini@gmail.com>
2026-07-16 20:41:43 +0530

    Time: 252 ms (13.86%), Space: 53.4 MB (13.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int dp[10001];
-    bool find(int idx, vector<int>& nums){
-        if(idx == nums.size()-1)return true;
-        if(idx >= nums.size())return false;
-        if(nums[idx] == 0)return false;
-        if(dp[idx] != -1)return dp[idx];
-        for(int i = 1; i <= nums[idx]; i++){
-            if(find(idx+i, nums)){
-               return dp[idx] = 1;
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)return true;
+        if(nums[0] == 0)return false;
+        vector<bool>reachEnd(n, false);
+        reachEnd[n-1] = true;
+        for(int i = n-2; i >= 0; i--){
+            if(i + nums[i] >= n-1){
+                reachEnd[i] = true;
+            }
+            else{
+                for(int j = i+1; j <= i+nums[i]; j++){
+                    if(reachEnd[j]){
+                        reachEnd[i] = true;
+                        break;
+                    }
+                }
             }
         }
-        return dp[idx] = 0;
-    }
-    bool canJump(vector<int>& nums) {
-        if(nums[0] == 0){
-            return nums.size() == 1;
-        }
-        memset(dp, -1, sizeof(dp));
-        return find(0, nums);
+        return reachEnd[0];
     }
 };
