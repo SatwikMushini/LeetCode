b8e21aea9d2d5ba0b52bdfa425b13c2ccfe5e52f
Satwik Mushini <satwikmushini@gmail.com>
2026-07-16 09:47:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.28%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -8,4 +8,5 @@ public:
     int gcdOfOddEvenSums(int n) {
         return gcd(n*n, n*(n+1));
     }
+    //https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fmeet%2F479242181793111%3Fp%3DEqvfYr0mDko46tVlwZ%26anon%3Dtrue&type=meet&deeplinkId=8ed69948-87f2-4c9c-bc22-d7728c4a54a3&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true
 };
