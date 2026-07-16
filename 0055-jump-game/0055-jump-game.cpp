089c8e156295aece8df678d5637c61d6a29fcdbc
Satwik Mushini <satwikmushini@gmail.com>
2026-07-16 20:04:30 +0530

    Time: 670 ms (5.34%), Space: 53.3 MB (13.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    unordered_map<int,bool>dp;
+    int dp[10001];
     bool find(int idx, vector<int>& nums){
         if(idx == nums.size()-1)return true;
         if(idx >= nums.size())return false;
         if(nums[idx] == 0)return false;
-        if(dp.find(idx) != dp.end())return dp[idx];
+        if(dp[idx] != -1)return dp[idx];
         for(int i = 1; i <= nums[idx]; i++){
             if(find(idx+i, nums))return dp[idx+i] = true;
         }
@@ -15,6 +15,7 @@ public:
         if(nums[0] == 0){
             return nums.size() == 1;
         }
+        memset(dp, -1, sizeof(dp));
         return find(0, nums);
     }
 };
