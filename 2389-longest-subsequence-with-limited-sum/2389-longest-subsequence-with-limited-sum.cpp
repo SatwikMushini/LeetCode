ecabb0ca055b81341bd11e3e1dd04599d6a60632
Satwik Mushini <satwikmushini@gmail.com>
2026-07-30 18:21:51 +0530

    Time: 3 ms (62.92%), Space: 18.2 MB (10.08%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    int query(vector<int>&bit, int idx){        //query to find the prefix sum, up until that pointt
-        int sum = 0;
-        while(idx > 0){
-            sum += bit[idx];
-            idx -= (idx & -idx);
-        }
-        return sum;
-    }
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        vector<int>bit(n+1, 0);
+        vector<int>prefix(n+1, 0);
 
-        for(int i = 0; i < n; i++){      //building fenwick tree
-            int num = nums[i], j = i + 1;
-            while(j <= n){
-                bit[j] += nums[i];
-                j += (j & -j);
-            }
+        for(int i = 0; i < n; i++){      
+            prefix[i+1] = prefix[i] + nums[i];
         }
 
         vector<int>ans;
@@ -27,8 +15,7 @@ public:
             int low = 1, high = n;
             while(low <= high){
                 int mid = low + (high - low) / 2;
-                int sum = query(bit, mid);
-                if(sum > x)high = mid-1;
+                if(prefix[mid] > x)high = mid-1;
                 else low = mid+1;
             }
             ans.push_back(high);
