fb72b675ddfeb6fe2c96df4a4bef84c65324e197
Satwik Mushini <satwikmushini@gmail.com>
2026-08-05 10:18:27 +0530

    Time: 420 ms (28.28%), Space: 359.5 MB (21.38%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& adjList, unordered_set<int>& sus){
+        sus.insert(node);
+        for(auto nbr : adjList[node]){
+            if(!sus.count(nbr))dfs(nbr, adjList, sus);
+        }
+    }
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>>adjList(n);
+
+        for(auto vec : invocations){            //O(n)
+            adjList[vec[0]].push_back(vec[1]);
+        }
+
+        unordered_set<int>sus;
+        dfs(k, adjList, sus);       //O(n)
+
+        vector<int>ans;
+        bool flag = true;
+
+        for(int i = 0; i < n; i++){ //O(n)
+            if(!sus.count(i)){
+                for(auto nbr : adjList[i]){
+                    if(sus.count(nbr)){ // doubt..
+                        flag = false; break;
+                    }
+                }
+                if(!flag)break;
+                else ans.push_back(i);
+            }
+        }
+        
+        if(flag)return ans;
+        ans.clear();
+        for(int i = 0; i < n; i++)ans.push_back(i); //O(n)
+        return ans;
+    }
+};
