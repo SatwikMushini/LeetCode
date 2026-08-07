50f56aa33d85d78d3c9ad35ea21d91514298db70
Satwik Mushini <satwikmushini@gmail.com>
2026-08-07 10:16:55 +0530

    Time: 0 ms (100%), Space: 31.3 MB (47.08%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] > target){
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        if(high == -1 || nums[high] != target)return -1;
+        return high;
+    }
+};
