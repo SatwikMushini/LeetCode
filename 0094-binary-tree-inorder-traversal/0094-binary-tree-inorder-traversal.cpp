93d4375fe3ad3aa76047a324c50b62020f06a346
Satwik Mushini <satwikmushini@gmail.com>
2026-08-10 15:08:36 +0530

    Time: 0 ms (100%), Space: 11.5 MB (7.39%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,27 +11,42 @@
  */
 class Solution {
 public:
-    
     vector<int> inorderTraversal(TreeNode* root) {
         if(!root)return {};
-        vector<int>ans;
-        
-        stack<TreeNode*>stk;
-        unordered_set<TreeNode*>seen;
 
+        vector<int>pre, ino, post;
+        unordered_set<TreeNode*>seenpre, seenino, seenpost;
+        stack<TreeNode*>stk;
         stk.push(root);
+
         while(!stk.empty()){
             TreeNode* node = stk.top();
-            if(!node->left || seen.count(node->left)){
-                ans.push_back(node->val);
-                seen.insert(node);
-                stk.pop();
-                if(node->right)stk.push(node->right);
+            if(!seenpre.count(node)){
+                pre.push_back(node->val);
+                seenpre.insert(node);
+                continue;
+            } 
+
+            if(!node->left || seenino.count(node->left)){
+                if(!seenino.count(node))ino.push_back(node->val);
+                seenino.insert(node);
             }
             else{
-                stk.push(node->left);
+                stk.push(node->left); continue;
             }
+            if((!node->left || seenpost.count(node->left)) 
+                && (!node->right || seenpost.count(node->right))){
+                post.push_back(node->val);
+                seenpost.insert(node);
+                stk.pop();
+            }
+            else{
+                stk.push(node->right);
+            }
+
         }
-        return ans;
+        vector<vector<int>> ans = {pre, ino, post};
+
+        return ino;
     }
 };
