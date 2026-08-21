862424ebafdd72f7ac9d546b74efca8e12ddb25a
Satwik Mushini <satwikmushini@gmail.com>
2026-08-21 11:18:30 +0530

    Time: 7 ms (5.19%), Space: 11.3 MB (5.59%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        sort(word.rbegin(), word.rend());
-
-        vector<int>freq(26, 0);
+        unordered_map<char,int>freq;
+        for(auto &x : word)freq[x]++;
         int cnt = 0;
-
-        for(auto x : word){
-            if(x >= 'a' && x <= 'z')freq[x - 'a']++;
-            else{
-                if(freq[x - 'A'] != -1 && freq[x - 'A']){
-                    cnt++;
-                    freq[x - 'A'] = -1;
-                }
-            }
+        for(int i = 'a'; i <= 'z'; i++){
+            if(freq[(char)i] && freq[toupper((char)i)])cnt++;
         }
         return cnt;
     }
