70a383e1bb5b29e9872d261bde67625483e4ff0e
Satwik Mushini <satwikmushini@gmail.com>
2026-09-13 18:41:20 +0530

    Time: 3 ms (26.95%), Space: 9.8 MB (82.38%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    void get(int idx, vector<int>& nums, vector<int>& ds, int n, vector<vector<int>>& ans){
-        if(idx == n){
-            ans.push_back(ds); return;
-        }
-
-        //pick
-        ds.push_back(nums[idx]);
-        get(idx+1, nums, ds, n, ans);
-        ds.pop_back();
-
-        // non pick
-        get(idx+1, nums, ds, n, ans);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        vector<int>ds;
-        get(0, nums, ds, nums.size(), ans);
+        for(int i = 0; i < (1 << n); i++){
+            vector<int>ds;
+            for(int j = 0; j < n; j++){
+                if(i & (1 << j))ds.push_back(nums[n-1-j]);
+            }
+            ans.push_back(ds);
+        }
         return ans;
     }
 };
