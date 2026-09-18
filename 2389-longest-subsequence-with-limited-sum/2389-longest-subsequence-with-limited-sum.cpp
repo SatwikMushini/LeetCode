9ee8b5a42030641150c7c45f2db78ac6ea968345
Satwik Mushini <satwikmushini@gmail.com>
2026-09-18 21:15:11 +0530

    Time: 1 ms (71.71%), Space: 17.8 MB (67.03%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -3,24 +3,31 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        vector<int>prefix(n+1, 0);
-
-        for(int i = 0; i < n; i++){      
-            prefix[i+1] = prefix[i] + nums[i];
+        for(int i = 1; i < n; i++){
+            nums[i] = nums[i] + nums[i-1];
         }
-
-        vector<int>ans;
-        for(auto x : queries){      
-            int low = 1, high = n;
-            while(low <= high){
-                int mid = low + (high - low) / 2;
-                if(prefix[mid] > x)high = mid-1;
+        int n2 = queries.size();
+        vector<int> ans;
+        for(int i = 0; i < n2; i++){
+            int query = queries[i];
+            int low = 0, high = n-1;
+            int idx = -1;
+            while(low < high){
+                int mid = low + (high-low)/2;
+                if(nums[mid] == query){
+                    idx = mid;
+                    break;
+                }
+                if(nums[mid] > query)high = mid-1;
                 else low = mid+1;
             }
-            ans.push_back(high);
+            if(idx != -1)ans.push_back(idx+1);
+            //else if(nums[low] == query)ans.push_back(low+1);
+            else{
+                if(nums[low] > query)ans.push_back(low);
+                else ans.push_back(low+1);
+            }
         }
-
         return ans;
     }
 };
